c9e047c91d937f2726d806484c0d2ac3295c0942
Siew Bin <tekito@kk.kk>
2020-05-12 13:18:49 +0900

    no message

---
+++ Source/DevilMansion/Enemy.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Enemy.generated.h"
+
+UCLASS()
+class DEVILMANSION_API AEnemy : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AEnemy();
+
+	UPROPERTY(EditAnywhere)
+	USkeletalMeshComponent* VisualMesh;
+
+	UPROPERTY(EditAnywhere)
+	float Movespeed;
+	UPROPERTY(EditAnywhere)
+	float Damage;
+	UPROPERTY(EditAnywhere)
+	float Cooldown;
+	UPROPERTY(EditAnywhere)
+	float Hitpoint;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
---
+++ Source/DevilMansion/Item.cpp
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Particles/ParticleSystem.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+#include "Engine/World.h"
+
+// Sets default values
+AItem::AItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionVolume = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionVolume"));
+	RootComponent = CollisionVolume;
+
+	VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	VisualMesh->SetupAttachment(GetRootComponent());
+
+	IdleParticles = CreateDefaultSubobject<UParticleSystemComponent>("IdleParticles");
+	IdleParticles->SetupAttachment(GetRootComponent());
+
+	bRotate = false;
+	RotationRate = 45.f;
+}
+
+// Called when the game starts or when spawned
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	CollisionVolume->OnComponentBeginOverlap.AddDynamic(this, &AItem::OnOverlapBegin);
+	CollisionVolume->OnComponentEndOverlap.AddDynamic(this, &AItem::OnOverlapEnd);
+}
+
+// Called every frame
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (bRotate)
+	{
+		FRotator Rotation = GetActorRotation();
+		Rotation.Yaw += DeltaTime * RotationRate;
+		SetActorRotation(Rotation);
+	}
+
+}
+
+void AItem::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Item::OnOverlapBegin()"));
+	if (OverlapParticles)
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), OverlapParticles, GetActorLocation(), FRotator(0.f), true);
+	}
+
+	if (OverlapSound)
+	{
+		UGameplayStatics::PlaySound2D(this, OverlapSound);
+	}
+
+	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr))
+	{
+		Destroy();
+		UE_LOG(LogTemp, Warning, TEXT("Destroy Item"));
+	}
+}
+
+void AItem::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Item::OnOverlapEnd()"));
+}
